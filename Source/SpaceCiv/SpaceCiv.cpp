6138e04ed5a7d3b085be2ee898d646f9241ef315
AntyThesis <163203888+AntyThesis@users.noreply.github.com>
2026-08-21 15:22:56 -0700

    Initial Unreal Engine project

---
+++ Source/SpaceCiv/SpaceCiv.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceCiv.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceCiv, "SpaceCiv" );
+ 
