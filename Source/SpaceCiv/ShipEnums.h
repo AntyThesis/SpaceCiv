8666f2d08e20dfaf8a30b158cf8d90efe945be76
AntyThesis <163203888+AntyThesis@users.noreply.github.com>
2026-08-22 14:30:48 -0700

    Added the ship structs and some relevant enums.

---
+++ Source/SpaceCiv/ShipEnums.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ShipEnums.generated.h"
+
+UENUM(BlueprintType)
+enum class EShipTypes : uint8
+{
+	SeaD	UMETA(DisplayName = "S.E.A.D"),
+	LandingCraft	UMETA(DisplayName = "Landing Craft"),
+	IndustrialShip	UMETA(DisplayName = "Industrial Ship"),
+	TransportShip	UMETA(DisplayName = "Transport Ship"),
+};
+
+UENUM(BlueprintType)
+enum class EFuelConsumption : uint8
+{
+	Low		UMETA(DisplayName = "Low"),
+	Medium	UMETA(DisplayName = "Medium"),
+	High		UMETA(DisplayName = "High")
+};
---
+++ Source/SpaceCiv/ShipStructs.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "ShipEnums.h"
+#include "ShipStructs.generated.h"
+
+USTRUCT(BlueprintType)
+struct FShipData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	
+
+	// Properties
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EShipTypes ShipTypes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Name;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* ShipSprite;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CargoSpace;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EFuelConsumption FuelConsumption;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Range;
+};
