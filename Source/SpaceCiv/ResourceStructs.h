36fff3dd258b8c138504ee4ed40b469f97394e76
AntyThesis <163203888+AntyThesis@users.noreply.github.com>
2026-08-25 22:35:57 -0700

    Added a data table to represent the resource data and hooked up the HUD

---
+++ Source/SpaceCiv/ResourceStructs.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+
+#include "CoreMinimal.h"
+#include "ResourceStructs.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FResourceData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	public:
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UTexture2D* Texture;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName Name;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FText Description;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float Amount;
+};
